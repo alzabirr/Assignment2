2ae0984c9d6d8afa6f19805350f77c3f07f71705
Al Zabir <121637305+alzabirr@users.noreply.github.com>
2024-08-23 08:09:14 -0700

    string.php

--- Assignment/string.php
+++ Assignment/string.php
@@ -1,2 +1,35 @@
 <?php
-echo "hello";
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+
+function countVowels($str) {
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $count = 0;
+    $str = strtolower($str);
+    for ($i = 0; $i < strlen($str); $i++) {
+        if (in_array($str[$i], $vowels)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+function reverseString($str) {
+    return strrev($str);
+}
+
+
+foreach ($strings as $string) {
+    
+    $vowelCount = countVowels($string);
+    
+    
+    $reversedString = reverseString($string);
+    
+    echo "Original String: $string\n";
+    echo "Vowel Count: $vowelCount\n";
+    echo "Reversed String: $reversedString\n";
+    echo "--------------------------\n";
+}
+?>
